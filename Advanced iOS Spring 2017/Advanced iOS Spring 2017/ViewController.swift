6a8c68d67acc92ef5870f712df7c98936ca1095c
mmaxxis <Tintu@Tin2.local>
2017-04-28 18:59:38 +0200

    cmmit first

--- Advanced iOS Spring 2017/Advanced iOS Spring 2017/ViewController.swift
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib. This is my first comment-Tin2
     }
 
     override func didReceiveMemoryWarning() {
